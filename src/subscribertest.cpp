f612ab351a2019a16627cd14ffb22e41ee14a487
Antoni Malecki <asdfirepl@gmail.com>
2018-12-11 18:13:00 +0100

    Subscriber now working, to start up *rosrun bookish_carnival subscriber*

--- src/subscribertest.cpp
+++ src/subscribertest.cpp
@@ -1,24 +1,21 @@
 #include "ros/ros.h"
 #include "std_msgs/Int8.h"
-int sound=-1;
+int sound = -1;
 void soundCallback(const std_msgs::Int8::ConstPtr& msg)
 {
+	ROS_INFO("Void function starts");
   sound = msg->data;
-  ROS_DEBUG("Playing sound: [%s]", sound);
+  ROS_INFO("Playing sound: %d", sound);
 }
 
 int main(int argc, char **argv)
 {
-
-  ros::init(argc, argv, "subscriber");
-
-  ros::NodeHandle nh;
-
-  ros::Subscriber sub = nh.subscribe("sound", 10, soundCallback);
-  while(ros::ok())
-  {
-  ros::spin();
-  ROS_INFO("Playing sound: ", sound);
-  }
+	ROS_INFO("starting up");
+  	ros::init(argc, argv, "subscriber");
+ 	ros::NodeHandle nh;
+  	ros::Subscriber sub = nh.subscribe("sound", 10, soundCallback);
+	ROS_INFO("starting loop", sound);
+	ros::spin();
+	// sound is a global variable that can now be used anywhere
   return 0;
 }
