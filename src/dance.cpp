8be6f01fcf3e5cad31b46f46c198fe896413292c
Antoni Malecki <asdfirepl@gmail.com>
2018-12-11 13:28:56 +0100

    Added the publisher part to the dance file.

--- src/dance.cpp
+++ src/dance.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "std_msgs/Int8.h"
 #include <stdlib.h>
 #include <time.h>
 
@@ -8,9 +9,13 @@ int main(int argc, char *argv[]) {
 
   ros::NodeHandle nh;
   ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10);
+  ros::Publisher sound_pub = nh.advertise<std_msgs::Int8>("sound", 10);
   srand (time(NULL));
   int mode = 0;
   geometry_msgs::Twist msg;
+  std_msgs::Int8 msgb;
+  msgb.data = 0;
+
   while(ros::ok())
   {
     mode = rand() % 3 + 0;
@@ -24,6 +29,8 @@ int main(int argc, char *argv[]) {
         msg.angular.z = 0.0;
         int tf=0;
         tf = rand() %10 + 3;
+        msgb.data = 1;
+        sound_pub.publish(msgb);
         for (int i=0 ; i<tf; i++)
           {
             cmd_vel_pub.publish(msg);
@@ -36,6 +43,8 @@ int main(int argc, char *argv[]) {
             cmd_vel_pub.publish(msg);
             ros::Duration(0.2).sleep();
         }
+      msgb.data = 0;
+      sound_pub.publish(msgb);
       }
       case 1:
       {
@@ -43,18 +52,15 @@ int main(int argc, char *argv[]) {
       msg.angular.z = 2.0;
       int ts=0;
       ts = rand() %10 + 3;
+      msgb.data = 3;
+      sound_pub.publish(msgb);
       for (int i=0 ; i<ts; i++)
         {
           cmd_vel_pub.publish(msg);
           ros::Duration(0.2).sleep();
         }
-      msg.linear.x = 0.0;
-      msg.angular.z = -2.0;
-      //for (int i=0 ; i<ts; i++)
-      //  {
-      //    cmd_vel_pub.publish(msg);
-      //    ros::Duration(0.2).sleep();
-      //  }
+        msgb.data = 0;
+        sound_pub.publish(msgb);
       }
       case 2:
       {
@@ -63,6 +69,8 @@ int main(int argc, char *argv[]) {
         msg.angular.z = 0.0;
         int tb=0;
         tb = rand() %10 + 3;
+        msgb.data = 2;
+        sound_pub.publish(msgb);
         for (int i=0 ; i<tb; i++)
           {
             cmd_vel_pub.publish(msg);
@@ -75,6 +83,8 @@ int main(int argc, char *argv[]) {
             cmd_vel_pub.publish(msg);
             ros::Duration(0.2).sleep();
         }
+        msgb.data = 0;
+        sound_pub.publish(msgb);
       }
       case 3:
       {
@@ -82,13 +92,15 @@ int main(int argc, char *argv[]) {
       msg.angular.z = -2.0;
       int ts=0;
       ts = rand() %10 + 3;
+      msgb.data = 3;
+      sound_pub.publish(msgb);
       for (int i=0 ; i<ts; i++)
         {
           cmd_vel_pub.publish(msg);
           ros::Duration(0.2).sleep();
         }
-      msg.linear.x = 0.0;
-      msg.angular.z = -2.0;
+      msgb.data = 0;
+      sound_pub.publish(msgb);
       }
 
     }
---
+++ src/sounds.cpp
@@ -0,0 +1,4 @@
+\\Nothing - 0
+\\Forward - 1
+\\Back - 2
+\\Spin - 3
