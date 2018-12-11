d3e892cf257a04fa8f1f272415ea58258c4e8121
Antoni Malecki <asdfirepl@gmail.com>
2018-12-11 11:38:41 +0100

    First working version

--- src/dance.cpp
+++ src/dance.cpp
@@ -1,37 +1,98 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
-#include "turtlesim/TeleportAbsolute.h"
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "bookish_carnival");
 
   ros::NodeHandle nh;
   ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
-
-  ros::Duration(1.0).sleep();
-
+  srand (time(NULL));
+  int mode = 0;
   geometry_msgs::Twist msg;
-  msg.linear.x = 1.0;
-  cmd_vel_pub.publish(msg);
+  while(ros::ok())
+  {
+    mode = rand() % 3 + 0;
 
-  ros::Duration(1.0).sleep();
+    switch(mode)
+    {
+      case 0:
+      {
+        //forward
+        msg.linear.x = 1.0;
+        msg.angular.z = 0.0;
+        int tf=0;
+        tf = rand() %10 + 3;
+        for (int i=0 ; i<tf; i++)
+          {
+            cmd_vel_pub.publish(msg);
+            ros::Duration(0.2).sleep();
+          }
+        msg.linear.x = -1.0;
+        msg.angular.z = 0.0;
+        for (int i=0 ; i<tf; i++)
+          {
+            cmd_vel_pub.publish(msg);
+            ros::Duration(0.2).sleep();
+        }
+      }
+      case 1:
+      {
+      msg.linear.x = 0.0;
+      msg.angular.z = 1.0;
+      int ts=0;
+      ts = rand() %10 + 3;
+      for (int i=0 ; i<ts; i++)
+        {
+          cmd_vel_pub.publish(msg);
+          ros::Duration(0.2).sleep();
+        }
+      msg.linear.x = 0.0;
+      msg.angular.z = -1.0;
+      //for (int i=0 ; i<ts; i++)
+      //  {
+      //    cmd_vel_pub.publish(msg);
+      //    ros::Duration(0.2).sleep();
+      //  }
+      }
+      case 2:
+      {
+        //back
+        msg.linear.x = -1.0;
+        msg.angular.z = 0.0;
+        int tb=0;
+        tb = rand() %10 + 3;
+        for (int i=0 ; i<tb; i++)
+          {
+            cmd_vel_pub.publish(msg);
+            ros::Duration(0.2).sleep();
+          }
+        msg.linear.x = 1.0;
+        msg.angular.z = 0.0;
+        for (int i=0 ; i<tb; i++)
+          {
+            cmd_vel_pub.publish(msg);
+            ros::Duration(0.2).sleep();
+        }
+      }
+      case 3:
+      {
+      msg.linear.x = 0.0;
+      msg.angular.z = -1.0;
+      int ts=0;
+      ts = rand() %10 + 3;
+      for (int i=0 ; i<ts; i++)
+        {
+          cmd_vel_pub.publish(msg);
+          ros::Duration(0.2).sleep();
+        }
+      msg.linear.x = 0.0;
+      msg.angular.z = -1.0;
+      }
 
-  msg.linear.x = 0.0;
-  msg.angular.z = 1.0;
-  cmd_vel_pub.publish(msg);
-
-  ros::Duration(1.0).sleep();
-
-  ros::ServiceClient client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
-
-  ros::Duration(1.0).sleep();
-
-  turtlesim::TeleportAbsolute srv;
-  srv.request.x = 1.0;
-  srv.request.y = 10.0;
-  client.call(srv);
-
-  ros::spin();
+    }
+  }
 
   return 0;
 }
