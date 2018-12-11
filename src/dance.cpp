c39ceb13aeae0b14583400d3bdc3c8b48fa0f7a7
Antoni Malecki <asdfirepl@gmail.com>
2018-12-11 11:03:48 +0100

    Fixed stuff that didn't work

--- src/dance.cpp
+++ src/dance.cpp
@@ -1,21 +1,37 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "turtlesim/TeleportAbsolute.h"
 
 int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "move_circle");
+  ros::init(argc, argv, "bookish_carnival");
 
   ros::NodeHandle nh;
-  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 5);
+  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+
+  ros::Duration(1.0).sleep();
 
   geometry_msgs::Twist msg;
-  msg.linear.x = 0.1;
-  msg.angular.z = 1.0;
+  msg.linear.x = 1.0;
+  cmd_vel_pub.publish(msg);
 
-  for(int; i=0; i<24; i++)
-  {
-    cmd_vel_pub.publish(msg);
-    ros::Duration(0.2).sleep();
-  }
+  ros::Duration(1.0).sleep();
+
+  msg.linear.x = 0.0;
+  msg.angular.z = 1.0;
+  cmd_vel_pub.publish(msg);
+
+  ros::Duration(1.0).sleep();
+
+  ros::ServiceClient client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
+
+  ros::Duration(1.0).sleep();
+
+  turtlesim::TeleportAbsolute srv;
+  srv.request.x = 1.0;
+  srv.request.y = 10.0;
+  client.call(srv);
+
+  ros::spin();
 
   return 0;
 }
