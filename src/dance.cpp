589ca75d1d7e9dbbbfb32820fe7c231ac9fc302e
Antoni Malecki <asdfirepl@gmail.com>
2018-12-12 13:59:06 +0100

    More cleaning up

--- src/dance.cpp
+++ src/dance.cpp
@@ -6,110 +6,112 @@
 
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "bookish_carnival");
-
   ros::NodeHandle nh;
-  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10);
-  ros::Publisher sound_pub = nh.advertise<std_msgs::Int8>("sound", 10);
-  srand (time(NULL));
-  int mode = 0;
+  ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10); //Setting up publisher for velocities
+  ros::Publisher sound_pub = nh.advertise<std_msgs::Int8>("sound", 10); //Setting up publisher used by player node
   geometry_msgs::Twist msg;
   std_msgs::Int8 msgb;
 
-  msgb.data = 0;
-
+  srand (time(NULL));
+  int mode = 0;
+  msgb.data = 0; //Publishing data for return / idle
+  sound_pub.publish(msgb);
   sleep(7);
+
   while(ros::ok())
-  {
-    mode = rand() % 3 + 0;
-
-    switch(mode)
     {
-      case 0:
-      {
-        //forward
-        msg.linear.x = 0.2;
-        msg.angular.z = 0.0;
-        int tf=0;
-        tf = rand() %20 + 6;
-        msgb.data = 1;
-        sound_pub.publish(msgb);
-        for (int i=0 ; i<tf; i++)
-          {
-            cmd_vel_pub.publish(msg);
-            ros::Duration(0.2).sleep();
-          }
-        msgb.data = 0;
-        sound_pub.publish(msgb);
-        msg.linear.x = -0.2;
-        msg.angular.z = 0.0;
-        for (int i=0 ; i<tf; i++)
-          {
-            cmd_vel_pub.publish(msg);
-            ros::Duration(0.2).sleep();
-        }
-
-
-      }
-      case 1:
-      {
-      msg.linear.x = 0.0;
-      msg.angular.z = 2.0;
-      int ts=0;
-      ts = rand() %20 + 6;
-      msgb.data = 3;
-      sound_pub.publish(msgb);
-      for (int i=0 ; i<ts; i++)
+      mode = rand() % 3 + 0;
+      switch(mode)
         {
-          cmd_vel_pub.publish(msg);
-          ros::Duration(0.2).sleep();
-        }
-        msgb.data = 0;
-        sound_pub.publish(msgb);
-      }
-      case 2:
-      {
-        //back
-        msg.linear.x = -0.2;
-        msg.angular.z = 0.0;
-        int tb=0;
-        tb = rand() %20 + 6;
-        msgb.data = 2;
-        sound_pub.publish(msgb);
-        for (int i=0 ; i<tb; i++)
-          {
-            cmd_vel_pub.publish(msg);
-            ros::Duration(0.2).sleep();
-          }
-        msgb.data = 0;
-        sound_pub.publish(msgb);
-        msg.linear.x = 0.2;
-        msg.angular.z = 0.0;
-        for (int i=0 ; i<tb; i++)
-          {
-            cmd_vel_pub.publish(msg);
-            ros::Duration(0.2).sleep();
-        }
+          case 0:
+          //Going forward
+            {
+              msg.linear.x = 0.2;
+              msg.angular.z = 0.0;
+              int tf=0;
+              tf = rand() %20 + 6;
+              msgb.data = 1; //Publishing data for going forward
+              sound_pub.publish(msgb);
 
-      }
-      case 3:
-      {
-      msg.linear.x = 0.0;
-      msg.angular.z = -2.0;
-      int ts=0;
-      ts = rand() %20 + 6;
-      msgb.data = 3;
-      sound_pub.publish(msgb);
-      for (int i=0 ; i<ts; i++)
-        {
-          cmd_vel_pub.publish(msg);
-          ros::Duration(0.2).sleep();
-        }
-      msgb.data = 0;
-      sound_pub.publish(msgb);
-      }
+              for (int i=0 ; i<tf; i++)
+                {
+                  cmd_vel_pub.publish(msg);
+                  ros::Duration(0.2).sleep();
+                }
 
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
+              msg.linear.x = -0.2; //Returning to initial position
+              msg.angular.z = 0.0;
+
+              for (int i=0 ; i<tf; i++)
+                {
+                  cmd_vel_pub.publish(msg);
+                  ros::Duration(0.2).sleep();
+                }
+            }
+          case 1:
+          //Spin direction 1
+            {
+              msg.linear.x = 0.0;
+              msg.angular.z = 2.0;
+              int ts=0;
+              ts = rand() %20 + 6;
+              msgb.data = 3; // Publishing data for spin
+              sound_pub.publish(msgb);
+              for (int i=0 ; i<ts; i++)
+                {
+                  cmd_vel_pub.publish(msg);
+                  ros::Duration(0.2).sleep();
+                }
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
+            }
+          case 2:
+          //Going backwards
+            {
+              msg.linear.x = -0.2;
+              msg.angular.z = 0.0;
+              int tb=0;
+              tb = rand() %20 + 6;
+              msgb.data = 2; //Publishing data for going backwards
+              sound_pub.publish(msgb);
+              for (int i=0 ; i<tb; i++)
+                {
+                  cmd_vel_pub.publish(msg);
+                  ros::Duration(0.2).sleep();
+                }
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
+              msg.linear.x = 0.2; //Returning to initial position
+              msg.angular.z = 0.0;
+              for (int i=0 ; i<tb; i++)
+                {
+                  cmd_vel_pub.publish(msg);
+                  ros::Duration(0.2).sleep();
+                }
+
+            }
+          case 3:
+          //Spin direction 2
+            {
+              msg.linear.x = 0.0;
+              msg.angular.z = -2.0;
+              int ts=0;
+              ts = rand() %20 + 6;
+              msgb.data = 3; // Publishing data for spin
+              sound_pub.publish(msgb);
+              for (int i=0 ; i<ts; i++)
+                {
+                  cmd_vel_pub.publish(msg);
+                  ros::Duration(0.2).sleep();
+                }
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
+            }
+
+        }
     }
-  }
 
   return 0;
 }
--- src/sounds.cpp
+++ src/sounds.cpp
@@ -2,66 +2,69 @@
 #include "std_msgs/Int8.h"
 #include <sound_play/sound_play.h>
 #include <unistd.h>
-int sound = -1;
-void soundCallback(const std_msgs::Int8::ConstPtr& msg)
+
+int sound = 0;
+
+void soundCallback(const std_msgs::Int8::ConstPtr& msg) //Callback function for subscriber
 {
-	ROS_INFO("Sound Callback");
   sound = msg->data;
-  ROS_INFO("Playing sound: %d", sound);
 }
 
-int main(int argc, char **argv)
-{
-	ROS_INFO("starting up");
-  	ros::init(argc, argv, "subscriber");
- 	ros::NodeHandle nh;
-  	ros::Subscriber sub = nh.subscribe("sound", 10, soundCallback);
-	ROS_INFO("starting loop", sound);
-	 sound_play::SoundClient sc;
-	while (ros::ok())
-	{
-	ros::spinOnce();
-	sleep(0.2);
-		switch(sound)
-		{
-		case(0):
-			{
-				sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/cricket.wav");
-     		while(sound == 0 && ros::ok())
-					{
-						sleep(0.1);
-						ros::spinOnce();
-					}
-			}
-		case(1):
-			{
-				sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/Yee.wav");
-				while(sound == 1 && ros::ok())
-					{
-						sleep(0.1);
-						ros::spinOnce();
-					}
-			}
-		case(2):
-			{
-				sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/reverse.wav");
-				while(sound == 2 && ros::ok())
-					{
-						sleep(0.1);
-						ros::spinOnce();
-					}
-			}
-		case(3):
-			{
-				sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/nani.wav");
-				while(sound == 3 && ros::ok())
-					{
-						sleep(0.1);
-						ros::spinOnce();
-					}
-			}
-		}
+int main(int argc, char **argv) {
 
-	}
+  ros::init(argc, argv, "subscriber");
+ 	ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("sound", 10, soundCallback);
+	sound_play::SoundClient sc;
+
+	while (ros::ok())
+		{
+			ros::spinOnce();
+			sleep(0.2);
+			switch(sound)
+				{
+					case(0):
+					//Idle / return sound
+						{
+							sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/cricket.wav");
+     					while(sound == 0 && ros::ok())
+								{
+									sleep(0.1);
+									ros::spinOnce();
+								}
+						}
+					case(1):
+					//Forward sound
+						{
+							sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/Yee.wav");
+							while(sound == 1 && ros::ok())
+								{
+									sleep(0.1);
+									ros::spinOnce();
+								}
+						}
+					case(2):
+					//Backwards sound
+						{
+							sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/reverse.wav");
+							while(sound == 2 && ros::ok())
+								{
+									sleep(0.1);
+									ros::spinOnce();
+								}
+						}
+					case(3):
+					//Spinning sound
+						{
+							sc.playWave("/home/ros/miniproject_ws/src/bookish_carnival/src/nani.wav");
+							while(sound == 3 && ros::ok())
+								{
+									sleep(0.1);
+									ros::spinOnce();
+								}
+						}
+				}
+
+		}
   return 0;
 }
