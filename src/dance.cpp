e32d8b62273b02ee4c7738889199661305df5001
ZoltanSzitas <31866158+zoltanszitas@users.noreply.github.com>
2018-12-12 15:08:54 +0100

    Go back!!!

--- src/dance.cpp
+++ src/dance.cpp
@@ -4,30 +4,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-void init()
-{
+int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "bookish_carnival");
   ros::NodeHandle nh;
   ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10); //Setting up publisher for velocities
   ros::Publisher sound_pub = nh.advertise<std_msgs::Int8>("sound", 10); //Setting up publisher used by player node
   geometry_msgs::Twist msg;
   std_msgs::Int8 msgb;
-}
-
-void move(float lin=0.0, float ang=0.0) //function for movement
-  {
-    msg.linear.x = lin;
-    msg.angular.z = ang;
-  }
-  
-void publishsound (int data=0)   //function for publishing sound data
-  {
-    msgb.data = data;  
-    sound_pub.publish(msgb);
-  }
-
-int main(int argc, char *argv[]) {
-  ros::init(argc, argv, "bookish_carnival");
-  init();
 
   srand (time(NULL));
   int mode = 0;
@@ -46,8 +29,10 @@ int main(int argc, char *argv[]) {
           case 0:
           //Going forward
             {
-              move(0.2,0.0);
-              publishsound(1); //Publishing data for going forward
+              msg.linear.x = 0.2;
+              msg.angular.z = 0.0;
+              msgb.data = 1; //Publishing data for going forward
+              sound_pub.publish(msgb);
 
               for (int i=0 ; i<t; i++)
                 {
@@ -55,8 +40,10 @@ int main(int argc, char *argv[]) {
                   ros::Duration(0.2).sleep();
                 }
 
-              publishsound(0); //Publishing data for return / idle
-              move(-0.2,0.0); //Returning to initial position
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
+              msg.linear.x = -0.2; //Returning to initial position
+              msg.angular.z = 0.0;
 
               for (int i=0 ; i<t; i++)
                 {
@@ -67,27 +54,34 @@ int main(int argc, char *argv[]) {
           case 1:
           //Spin direction 1
             {
-              move(0.0,2.0);
-              publishsound(3); // Publishing data for spin
+              msg.linear.x = 0.0;
+              msg.angular.z = 2.0;
+              msgb.data = 3; // Publishing data for spin
+              sound_pub.publish(msgb);
               for (int i=0 ; i<t; i++)
                 {
                   cmd_vel_pub.publish(msg);
                   ros::Duration(0.2).sleep();
                 }
-              publishsound(0); //Publishing data for return / idle
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
             }
           case 2:
           //Going backwards
             {
-              move(-0.2,0.0);
-              publishsound(2); //Publishing data for going backwards
+              msg.linear.x = -0.2;
+              msg.angular.z = 0.0;
+              msgb.data = 2; //Publishing data for going backwards
+              sound_pub.publish(msgb);
               for (int i=0 ; i<t; i++)
                 {
                   cmd_vel_pub.publish(msg);
                   ros::Duration(0.2).sleep();
                 }
-              publishsound(0); //Publishing data for return / idle
-              move(0.2,0.0); //Returning to initial position
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
+              msg.linear.x = 0.2; //Returning to initial position
+              msg.angular.z = 0.0;
               for (int i=0 ; i<t; i++)
                 {
                   cmd_vel_pub.publish(msg);
@@ -98,14 +92,17 @@ int main(int argc, char *argv[]) {
           case 3:
           //Spin direction 2
             {
-              move(0.0,-0.2);
-              publishsound(3); // Publishing data for spin
+              msg.linear.x = 0.0;
+              msg.angular.z = -2.0;
+              msgb.data = 3; // Publishing data for spin
+              sound_pub.publish(msgb);
               for (int i=0 ; i<t; i++)
                 {
                   cmd_vel_pub.publish(msg);
                   ros::Duration(0.2).sleep();
                 }
-              publishsound(0); //Publishing data for return / idle
+              msgb.data = 0; //Publishing data for return / idle
+              sound_pub.publish(msgb);
             }
 
         }
